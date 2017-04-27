ffac6fcc2e4d616e26673b99eb86012fbd88ff4d
Josh Holguin <josh.holguin@springbox.com>
2017-04-27 08:54:21 -0500

    Calculator Challenge

---
+++ calculator.js
@@ -0,0 +1,198 @@
+/**
+ * @description Calculator prototype.
+ * @constructor Calculator
+ */
+function Calculator()
+{
+    /**
+     * @description Assign this to variable for scope scenarios.
+     * @type {Calculator}
+     */
+    var self = this
+        ,numMap = [1,2,3,4,5,6,7,8,9,"clear",0,"="]
+        ,operands = [
+            {
+                icon: '+',
+                iconname: 'icon-plus'
+            },
+            {
+                icon: '-',
+                iconname: 'icon-minus'
+            },
+            {
+                icon: '*',
+                iconname: 'icon-multiplication'
+            },
+            {
+                icon: '/',
+                iconname: 'icon-division'
+            },
+            {
+                icon: '^',
+                iconname: 'icon-square'
+            }
+        ]
+        ,opWindow
+        ,equalSelected = false
+        ,equationsStore = [];
+
+
+    /**
+     * @description Init function builds the calculator.
+     */
+    this.init = function() {
+        initLocalStorage();
+        var container = document.createElement("div");
+        container.id = "main";
+
+        opWindow = document.createElement("div");
+        opWindow.id = "opWindow";
+        container.prepend(opWindow);
+
+        buildOperands(container);
+        buildNumbers(container);
+
+        document.body.prepend(container);
+        
+    };
+    
+    /**
+     * @description initLocalStorage function checks if local storage is valid and sets the equation store to
+     * value stored in localStorage
+     */
+    function initLocalStorage(){
+        if(typeof(Storage) !== "undefined"){
+            equationsStore = (localStorage.hasOwnProperty('equations')) ? JSON.parse(localStorage.equations) : [];
+            populateSavedEquations();
+        }else{
+            console.log("No Local Storage for you!!!")
+        }
+    }
+
+    /**
+     * @description populateSavedEquations function builds Stored equations container 
+     */
+    function populateSavedEquations(){
+        var savedContainer = document.createElement("section");
+        savedContainer.className = "equations-stored";
+        var title = document.createElement("h3");
+        title.innerHTML = "Saved Equations";
+        savedContainer.append(title);
+
+        
+        equationsStore.forEach(function(val){
+            var eq = document.createElement("span");
+            eq.innerHTML = val + '=' + eval(val);
+            savedContainer.append(eq);
+        });
+
+        if(equationsStore.length == 0){
+            var eq = document.createElement("span");
+            eq.innerHTML = "No saved equations";
+            eq.className = "no-equations";
+            savedContainer.append(eq);
+        }
+
+        document.body.prepend(savedContainer);
+
+    }
+    
+    /**
+     * @description buildOperands function Builds Operands section
+     */
+    function buildOperands(container){
+        var operandsContainer = document.createElement("div");
+        operandsContainer.id = "ops-container";
+
+        operands.forEach(function(val){
+            var button = document.createElement("button");
+            button.className = val.iconname;
+            button.value = val.icon;
+            button.addEventListener("click", handleClick);
+            operandsContainer.append(button);
+        });
+
+        container.append(operandsContainer);
+    }
+
+    /**
+     * @description buildNumbers function Builds Number keys
+     */
+    function buildNumbers(container){
+        var numberContainer = document.createElement("section");
+        numberContainer.id = "number-container";
+
+
+        numMap.forEach(function(val,index){
+            
+            var button = document.createElement("button");
+            button.innerHTML = val;
+            button.value = val;
+            button.addEventListener("click", handleClick);
+
+            numberContainer.append(button);
+        });
+
+        container.append(numberContainer);
+    }
+
+     /**
+     * @description saveEquation function utilitzes local stroage to save equations that are being solved
+     */
+    function saveEquation(equation){
+        equationsStore.push(equation);
+
+        if(typeof(Storage) !== "undefined"){
+            localStorage.equations = JSON.stringify(equationsStore);
+        }else{
+            console.log("No Local Storage for you!!!")
+        }
+    }
+
+
+    /**
+     * @description clearOpWindow function clears output window
+     */
+    function clearOpWindow(){
+        opWindow.innerHTML = "";
+        equalSelected = false;
+    }
+
+
+    /**
+     * @description handleClick function handles button clicks
+     */
+    function handleClick(event){
+        event.preventDefault();
+        
+        switch(event.target.value){
+            case "=":
+                if(!equalSelected){
+                    saveEquation(opWindow.innerHTML);
+                    opWindow.innerHTML = eval(opWindow.innerHTML);
+                    equalSelected = true;
+                }else{
+                    clearOpWindow();
+                }
+                
+                break;
+            case "clear":
+                clearOpWindow();
+                break;
+            case "^":
+                saveEquation(opWindow.innerHTML);
+                opWindow.innerHTML = Math.pow(opWindow.innerHTML, 2);
+                equalSelected = true;
+                break;
+            default:
+                if(!equalSelected)
+                    opWindow.innerHTML += event.target.value;
+                else{
+                    opWindow.innerHTML = event.target.value;
+                    equalSelected = false;
+                }
+
+        }
+    }
+
+}
